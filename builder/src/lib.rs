972be58f48d89ae99ba33409bfe1d36f126dab47
kanaru0928 <kanaru0928@gmail.com>
2024-08-23 00:08:25 +0900

    feat(builder): test01

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,9 @@
 use proc_macro::TokenStream;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+    let _parsed = parse_macro_input!(input as DeriveInput);
+    
+    TokenStream::new()
 }
